259cb02a2188c774efd5821943a8240c16f93160
Jason Partyka <4501353+jasonpartyka@users.noreply.github.com>
2022-04-05 13:20:54 -0500

    🤷🏻‍♂️

--- tests/nightwatch.windows.conf.js
+++ tests/nightwatch.windows.conf.js
@@ -69,7 +69,7 @@ module.exports = {
       },
       webdriver: {
         start_process: true,
-        server_path: './node_modules/geckodriver/bin/geckodriver',
+        server_path: '.\node_modules\geckodriver\bin\geckodriver.exe',
         cli_args: [
           // very verbose geckodriver logs
           // '-vv'
