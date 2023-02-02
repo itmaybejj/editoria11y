1836e83e4375a440e8b2f306552ec7127b9dd82c
John Jameson <jjameson@princeton.edu>
2023-02-02 10:38:39 -0500

    Update pack.sh

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is a simple script to pull down the specified version of editoria11y from github
+# Format JS, build .min file and run tests
 
 npx eslint ./js --fix
 uglifyjs ./js/{ed11y-localization,ed11y-element-alt,ed11y-element-panel,ed11y-element-result,ed11y-element-tip,ed11y-test-embeds,ed11y-test-headings,ed11y-test-images,ed11y-test-links,ed11y-test-text,ed11y}.js -c -m --output dist/editoria11y.min.js
