990e680c11f0697f403e53c98f7c265caf3bc8ec
John Jameson <jjameson@princeton.edu>
2023-11-14 14:05:41 -0500

    Calculate using treewalker.

--- js/ed11y.js
+++ js/ed11y.js
@@ -503,55 +503,6 @@ class Ed11y {
       return linkText;
     };
 
-    // Handle aria-label or labelled-by. Latter "wins" and can self-label.
-    Ed11y.computeAriaLabel = function (el, recursing = false) {
-      let label = el.hasAttribute('aria-label');
-      let labelledBy = el.hasAttribute('aria-labelledby');
-
-      if (!recursing) {
-        // On first pass only, we compute labelledby and recurse if applicable.
-        if (labelledBy) {
-          let idList = el.getAttribute('aria-labelledby');
-          let returnText = '';
-          if (idList.length > 0) {
-            idList = '#' + idList;
-            idList = idList.replace(/ /g, ', #');
-            let targets = document.querySelectorAll(idList);
-            targets?.forEach((target) => {
-              returnText += Ed11y.computeAriaLabel(target, true) + ' ';
-            });
-          }
-          return returnText;
-        } else if (!label) {
-          // No aria found.
-          return 'noAria';
-        }
-      }
-
-      // When no labelledby and not recursing, return label if exists.
-      if (label) {
-        return el.getAttribute('aria-label');
-      } else if (recursing) {
-        // Todo: rest of naming algorithm? Title etc?
-        // In loop, labelledBy should populate with element text.
-        return Ed11y.getText(el);
-      }
-    };
-
-    // recursively look for titles
-    Ed11y.computeTitle = function (el) {
-      // todo beta replace with Sa11y name calculation
-      if (el.hasAttribute('title')) {
-        return el.getAttribute('title');
-      }
-      else if (el.querySelector('[title]')) {
-        return el.querySelector('[title]').getAttribute('title');
-      }
-      else {
-        return '';
-      }
-    };
-
     // QuerySelectAll non-ignored elements within checkroots, with recursion into shadow components
     Ed11y.findElements = function (key, selector, rootRestrict = true) {
 
@@ -993,7 +944,7 @@ class Ed11y {
           levelPrefix.textContent = `H${level}: `;
           li.append(levelPrefix);
           let userText = document.createElement('span');
-          userText.textContent = el[0].textContent;
+          userText.textContent = Ed11y.computeText(el[0]);
           li.append(userText);
           if (el[2]) { // Has an error message
             let type = !el[3] ? 'error' : 'warning';
@@ -1226,6 +1177,190 @@ class Ed11y {
       return next;
     };
 
+    // Handle aria-label or labelled-by. Latter "wins" and can self-label.
+    Ed11y.computeAriaLabel = function (el, recursing = false) {
+      let labelledBy = el.getAttribute('aria-labelledby');
+      if (!recursing && labelledBy) {
+        // On first pass only, we compute labelledby and recurse if applicable.
+        let returnText = '';
+        if (labelledBy.trim().length > 0) {
+          let idList = labelledBy.trim().replace(/ /g, ', #');
+          let targets = document.querySelectorAll(`#${CSS.escape(idList)}`);
+          console.warn(targets);
+          if (targets.length > 0) {
+            targets?.forEach((target) => {
+              returnText += Ed11y.computeText(target, 1) + ' ';
+            });
+            console.warn('returning: ' + returnText);
+            console.log(Ed11y.trees[0]);
+            console.log(Ed11y.trees[1]);
+            return returnText;
+          }
+        }
+      } 
+      if (el.ariaLabel && el.ariaLabel.trim().length > 0) {
+        // todo add empty and whitespace string tests
+        return el.ariaLabel;
+      }
+      console.log('end of the line for');
+      console.log(el);
+      return 'noAria';
+    };
+
+    // recursively look for titles
+    Ed11y.computeTitle = function (el) {
+      // todo beta replace with Sa11y name calculation
+      if (el.hasAttribute('title')) {
+        return ' ' + el.getAttribute('title');
+      }
+      return '';
+    };
+
+    // todo: remove.
+    Ed11y.nextTreeBranch = function(tree) {
+      for (let i = 0; i < 1000; i++) {
+        if (tree.nextSibling()) {
+          // Prepare for continue to advance.
+          return tree.previousNode();
+        }
+        // Next node will be in next branch.
+        if (!tree.parentNode()) {
+          return false;
+        }
+      }
+      return false;
+    };
+
+    Ed11y.getPseudoContent = function(el) {
+      let before = window.getComputedStyle(
+        el, ':before'
+      ).getPropertyValue('content');
+      let after = window.getComputedStyle(
+        el, ':after'
+      ).getPropertyValue('content');
+      return [before === 'none' ? '' : before.replace(/^"(.*)"$/, '$1'), after === 'none' ? '' : after.replace(/^"(.*)"$/, '$1')];
+    };
+
+    Ed11y.trees = [];
+    Ed11y.computeText = function (el, recursing = 0) {
+      
+      // Return immediately if there is an aria label.
+      let hasAria = Ed11y.computeAriaLabel(el, recursing);
+      if (hasAria !== 'noAria') {
+        console.warn('early intercept: ' + hasAria);
+        return hasAria;
+      }
+
+      // Return immediately if there is only a text node.
+      let computedText = '';
+      let pseudoCSS = Ed11y.getPseudoContent(el);
+      if (!el.children.length) {
+        // Just text! Output immediately.
+        computedText = el.textContent;
+        computedText = pseudoCSS[0] + computedText + pseudoCSS[1];
+        if (!computedText && el.hasAttribute('title')) {
+          return el.getAttribute('title');
+        }
+        return computedText.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+      }
+
+      // Otherwise, recurse into children.
+      Ed11y.trees[recursing] = document.createTreeWalker(
+        el,
+        NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT
+      );
+      
+      let addTitleIfNoName = false;
+      let aText = false;
+      let count = 0;
+
+      while (Ed11y.trees[recursing].nextNode()) {
+        count++;
+        console.log(Ed11y.trees[recursing]);
+        // Links can use title attribute as text.
+        if (addTitleIfNoName && !Ed11y.trees[recursing].currentNode.closest('a')) {
+          if (aText === computedText) {
+            computedText += addTitleIfNoName;
+          }
+          addTitleIfNoName = false;
+          aText = false;
+        }
+        if (Ed11y.trees[recursing].currentNode.nodeType === Node.TEXT_NODE) {
+          computedText += Ed11y.trees[recursing].currentNode.nodeValue;
+          continue;
+        }
+        if (Ed11y.trees[recursing].currentNode.hasAttribute('aria-hidden') && !(recursing && count < 3)) {
+          // Ignore elements and children, except when directly aria-referenced.
+          // W3C name calc 2 is more complicated than this, but this is good enough.
+          if (!Ed11y.nextTreeBranch(Ed11y.trees[recursing])) {
+            break;
+          }
+          continue;
+        }
+        let aria = 'noAria';
+        //let aria = Ed11y.computeAriaLabel(Ed11y.trees[recursing].currentNode, recursing);
+        console.log(recursing);
+        
+        console.log('aria return is ' + Ed11y.computeAriaLabel(Ed11y.trees[recursing].currentNode, recursing));
+        if (aria !== 'noAria') {
+          computedText += aria;
+          console.log('p1');
+          if (!Ed11y.nextTreeBranch(Ed11y.trees[recursing])) {
+            break;
+          }
+          console.log('p2');
+          continue;
+        }
+
+        let pseudoText = Ed11y.getPseudoContent(Ed11y.trees[recursing].currentNode);
+        switch (Ed11y.trees[recursing].currentNode.tagName) {
+        case 'STYLE':
+          // Skip style elements
+          if (!Ed11y.nextTreeBranch(Ed11y.trees[recursing])) {
+            break;
+          }
+          continue;
+        case 'IMG':
+          if (Ed11y.trees[recursing].currentNode.hasAttribute('alt')) {
+            computedText += Ed11y.trees[recursing].currentNode.getAttribute('alt');
+          }
+          computedText = pseudoText[0] + computedText + pseudoText[1];
+          continue;
+        case 'SVG':
+          if (Ed11y.trees[recursing].currentNode.getAttribute('role') === 'image' && Ed11y.trees[recursing].currentNode.hasAttribute('alt')) {
+            computedText += Ed11y.trees[recursing].currentNode.getAttribute('alt');
+            computedText = pseudoText[0] + computedText + pseudoText[1];
+            if (!Ed11y.nextTreeBranch(Ed11y.trees[recursing])) {
+              break;
+            }
+          }
+          continue;
+        case 'A':
+          if (el.hasAttribute('title')) {
+            addTitleIfNoName = el.getAttribute('title');
+            aText = computedText;
+          }
+          computedText = pseudoText[0] + computedText + pseudoText[1];
+          break;
+        default:
+          // Other tags continue as-is.
+          computedText = pseudoText[0] + computedText + pseudoText[1];
+          break;
+        }
+      }
+      // At end of loop, add last title element if need be.
+      if (addTitleIfNoName && !aText) {
+        computedText += addTitleIfNoName;
+      }
+      
+      if (!computedText && el.hasAttribute('title')) {
+        return el.getAttribute('title');
+      }
+      return computedText.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+
+    };
+
+    // Use for BASIC testing or comparing. Otherwise use computeText().
     Ed11y.getText = function (el) {
       return el.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
     };
