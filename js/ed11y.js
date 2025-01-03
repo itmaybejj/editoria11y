4b30cd06d36e365b51f94c06304c5571a7d5de19
John Jameson <jjameson@princeton.edu>
2025-01-03 14:51:17 -0500

    bring back preload

--- js/ed11y.js
+++ js/ed11y.js
@@ -753,9 +753,7 @@ class Ed11y {
         // [5] dismissalStatus
         */
       let mark = document.createElement('ed11y-element-result');
-      if (!Ed11y.incremental) {
-        mark.classList.add('ed11y-preload');
-      }
+      mark.classList.add('ed11y-preload');
       let location;
       let position = 'beforebegin';
       mark.setAttribute('id', 'ed11y-result-' + index);
@@ -1346,13 +1344,11 @@ class Ed11y {
 
         });
       }
-      window.setTimeout(()=> {
-        Ed11y.jumpList?.forEach(mark => {
-          // Now make visible.
-          // todo: this still doesn't work in Drupal live edit mode.
-          mark.classList.remove('ed11y-preload');
-        });
-      },10);
+      Ed11y.jumpList?.forEach(mark => {
+        // Now make visible.
+        // todo: Edge still flickers on redraw.
+        mark.classList.remove('ed11y-preload');
+      });
     };
 
 
