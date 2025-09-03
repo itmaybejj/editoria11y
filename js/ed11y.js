bf9b7a852f2e4f1962dda70183912a7f40188ba5
John Jameson <jjameson@princeton.edu>
2025-09-03 11:14:53 -0400

    Pull missing jumplist commit from 2.3

--- js/ed11y.js
+++ js/ed11y.js
@@ -1369,8 +1369,7 @@ class Ed11y {
     Ed11y.positionedFrames = [];
 
     Ed11y.alignButtons = function () {
-
-      if (Ed11y.jumpList.length === 0 || (Ed11y.openTip.button && Ed11y.scrollPending === 0)) {
+      if (!Ed11y.jumpList || Ed11y.jumpList.length === 0 || (Ed11y.openTip.button && Ed11y.scrollPending === 0)) {
         return;
       }
       Ed11y.alignPending = true;
