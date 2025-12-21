2720b62ab08fbb5c375901493f96c1cc30ca612f
John Jameson <jjameson@princeton.edu>
2025-12-21 07:23:30 -0500

    match with merge.

--- scripts/get.sh
+++ scripts/get.sh
@@ -12,9 +12,6 @@ mv src/js ../src/sa11y
 # Patches
 # Don't inject Sa11y CSS into shadow components
 cp ../src/js-overrides/logic/find-shadow-components.js ../src/sa11y/logic/find-shadow-components.js
-# Don't instantiate the Sa11y readability panel.
-# Don't import any APCA nonCommercial licensed code.
-# cp ../src/js-overrides/utils/contrast-utils.js ../src/sa11y/utils/contrast-utils.js
 
 cd ../
 rm -rf tmp
