fda33a4ff2b5bd2c362b513a1411db3cc3f92674
John Jameson <jjameson@princeton.edu>
2026-04-05 17:21:16 -0400

    workflow

--- scripts/pack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Format JS, build .min file and run tests
-
-npx eslint ./js/** --fix
-./node_modules/.bin/css-minify -d ./css -o ./dist
-# npx nightwatch tests --workers=4 --env=chrome
