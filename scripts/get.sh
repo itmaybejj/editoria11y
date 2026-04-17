e4703bf4ff42ff0d8d9fb5733439ea65cc7aa8cc
John Jameson <jjameson@princeton.edu>
2026-04-17 06:15:48 -0400

    Sa11y 5.0

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This is a simple script to pull down the specified Sa11y branch from github
-GIT_REF="dev-4.4.2"
+GIT_REF="5.0.0"
 mkdir -p tmp/
 cd tmp/
 git clone git@github.com:ryersondmp/sa11y.git .
